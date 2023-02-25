93193443d218f2070bbe7e11bf4c0000cdab2010
loiodicj1 <loiodicj1@newpaltz.edu>
2023-02-25 15:00:55 -0500

    SignInView Prototype

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeView from '../views/WelcomeView.vue'
+import SignInView from '../views/SignInView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: WelcomeView
+      component: SignInView
     },
   ]
 })
---
+++ client/src/views/SignInView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  //someone needs to come up with something less cringe
+  const messages: String[] = [
+    "we're glad you're back",
+    "we missed you",
+    "uh-oh, what did you buy?",
+    "tell us your secrets"
+  ];
+</script>
+
+<template>
+  <main>
+    <div class="box is-centered has-text-centered">
+        <div class="title has-text-primary">sign in</div>
+        <div class="subtitle has-text-info"> {{ messages[Math.floor(Math.random() * messages.length)] }} </div>
+
+        <div class="field">
+          <p class="control has-icons-left has-icons-right">
+            <input class="input is-primary has-text-info" type="email" placeholder="email">
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input is-primary has-text-info" type="password" placeholder="password">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+
+        <div class="button is-light is-primary" @click="">go</div>
+
+        <div class="container level-right">
+          <div class="button is-link is-underlined is-inverted" @click="">sign up</div>
+        </div>
+      </div>
+  </main>
+</template>
+
+<style>
+</style>
