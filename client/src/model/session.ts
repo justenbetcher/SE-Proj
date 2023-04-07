03d7f659742790d104158057345b327490dd6182
Justen Betcher <betcherj1@newpaltz.edu>
2023-04-07 11:26:01 -0400

    added budget interface to User interface

--- client/src/model/session.ts
+++ client/src/model/session.ts
@@ -1,5 +1,5 @@
 import { reactive} from 'vue';
-import { type Entry } from './tracker';
+import type { Entry }  from './tracker';
 
 
 const session = reactive({
@@ -8,13 +8,23 @@ const session = reactive({
 
 interface User {
     name: string;
-    profileID?: number;
+    profileID?: string;
     email?: string;
     password?: string;
     profilePicture?: string;
     pastEntrys?: Entry[];
+    budget?:  Budget;
 }
 
+interface Budget {
+    salaryOrHourly: boolean;
+    salary?: number;
+    variableIncome?: number; // for the people who arnt on a salary
+    budgetPercentage?: number; // percentage of income people want to save
+
+}
+
+// sessoin methods
 export function useSession() {
     return session;
 }
@@ -22,5 +32,22 @@ export function useSession() {
 export function login() {
     session.currentUser = {
         name: 'john doe',
+    }
+}
+
+export function setUserId() {
+    if (session.currentUser)
+    {
+        session.currentUser.profileID 
+        = session.currentUser.name.replace(/ /g, '').toLowerCase();
+    }
+}
+
+// budget methods
+
+export function preSetBudget() {
+    if(session.currentUser)
+    {
+        session.currentUser.budget = {} as Budget;
     }
 }
