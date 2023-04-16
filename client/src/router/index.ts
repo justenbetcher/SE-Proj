9e282150c5b9e2198d57ef7e24519ae1da14f4cf
loiodicj1 <loiodicj1@newpaltz.edu>
2023-04-16 11:24:09 -0400

    BudgetView in router

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomeView from '../views/WelcomeView.vue'
 import SignInView from '../views/SignInView.vue'
-import HomeView from '../views/HomeView.vue'
+import BudgetView from '../views/BudgetView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,9 +11,9 @@ const router = createRouter({
       component: SignInView
     },
     {
-      path: '/home',
-      name: 'Home',
-      component: HomeView
+      path: '/budget',
+      name: 'budget',
+      component: BudgetView
     },
   ]
 })
